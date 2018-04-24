d2c58b81c0292e8d74e90343de1e410ea235a08d
Miguel <migherdcas@gmail.com>
2018-04-24 20:37:03 +0200

    Normalize-scss y Autoprefixer incluido con gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var normalize = require('node-normalize-scss');
 
 // Static Server + watching scss/html files
 gulp.task('serve', function () {
@@ -13,15 +15,22 @@ gulp.task('serve', function () {
         }
     });
 
-    gulp.watch("./app/sass/**/*.scss", ['sass']);
+    gulp.watch("./app/assets/styles/sass/**/*.scss", ['sass']);
     gulp.watch("./app/index.html").on('change', browserSync.reload);
+
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-    return gulp.src("./app/sass/main.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("./app/css"))
+    return gulp.src("./app/assets/styles/sass/main.scss")
+        .pipe(sass({
+            includePaths: require('node-normalize-scss').includePaths
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', '> 5%', 'Firefox ESR'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("./app/temp/styles/"))
         .pipe(browserSync.stream());
 });
 
